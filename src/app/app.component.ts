24c62bfba96df1d3c0be6b5e9e4caf6ef6c70406
Roman Kazarov <rkazarov@beyondtrust.com>
2021-01-20 21:08:46 +0200

    2.11 view child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild('appTitle')
+  title!: ElementRef;
+
+  ngAfterViewInit(): void {
+    this.title.nativeElement.textContent = 'Shop';
+  }
+
 }
