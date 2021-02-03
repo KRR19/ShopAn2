775fcc0a31d66d501beadb16b0f295fd7c338edc
Roman <kazarovroman@gmail.com>
2021-02-03 21:18:30 +0200

    5.1

--- src/app/product/product-routing.module.ts
+++ src/app/product/product-routing.module.ts
@@ -1,7 +1,10 @@
+import { ProductListComponent } from './Components/product-list/product-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'product-list', component: ProductListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
